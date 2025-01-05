4a9e3123e69e673a29e69b3e23aca8280bac7530
cdnss <32522595+cdnss@users.noreply.github.com>
2025-01-05 09:12:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,74 +1,119 @@
-from selenium import webdriver
-from selenium_stealth import stealth
-from bs4 import BeautifulSoup
-from flask import *
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
 import os
-app = Flask(__name__)
-ports = os.getenv("PORT", 5000)
+import subprocess
+import shutil
+
+CRD_SSH_Code = input("Google CRD SSH Code :")
+username = "user" #@param {type:"string"}
+password = "root" #@param {type:"string"}
+os.system(f"useradd -m {username}")
+os.system(f"adduser {username} sudo")
+os.system(f"echo '{username}:{password}' | sudo chpasswd")
+os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
+
+Pin = 123456 #@param {type: "integer"}
+Autostart = True #@param {type: "boolean"}
+
+class CRDSetup:
+    def __init__(self, user):
+        os.system("apt update")
+        self.installCRD()
+        self.installDesktopEnvironment()
+        self.changewall()
+        self.installGoogleChrome()
+        self.installTelegram()
+        self.installQbit()
+        self.finish(user)
+
+    @staticmethod
+    def installCRD():
+        subprocess.run(['wget', 'https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'])
+        subprocess.run(['dpkg', '--install', 'chrome-remote-desktop_current_amd64.deb'])
+        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'])
+        print("Chrome Remoted Desktop Installed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+    @staticmethod
+    def installDesktopEnvironment():
+        os.system("export DEBIAN_FRONTEND=noninteractive")
+        os.system("apt install --assume-yes xfce4 desktop-base xfce4-terminal")
+        os.system("bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remote-desktop-session'")
+        os.system("apt remove --assume-yes gnome-terminal")
+        os.system("apt install --assume-yes xscreensaver")
+        os.system("sudo service lightdm stop")
+        os.system("sudo apt-get install dbus-x11 -y")
+        os.system("service dbus start")
+        print("Installed XFCE4 Desktop Environment !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+    @staticmethod
+    def installGoogleChrome():
+        subprocess.run(["wget", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"])
+        subprocess.run(["dpkg", "--install", "google-chrome-stable_current_amd64.deb"])
+        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'])
+        print("Google Chrome Installed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    
+    @staticmethod
+    def installTelegram():
+        
+        print("Telegram Installed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+    @staticmethod
+    def changewall():
+        
+        print("Wallpaper Changed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
    
+    @staticmethod
+    def installQbit():
 
+        print("Qbittorrent Installed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
-target = "https://doujindesu.tv"
+    @staticmethod
+    def finish(user):
+        if Autostart:
+            os.makedirs(f"/home/{user}/.config/autostart", exist_ok=True)
+            link = "www.youtube.com/@The_Disala"
+            colab_autostart = """[Desktop Entry]
+            print("Finalizing !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
+Type=Application
+Name=Colab
+Exec=sh -c "sensible-browser {}"
+Icon=
+Comment=Open a predefined notebook at session signin.
+X-GNOME-Autostart-enabled=true""".format(link)
+            with open(f"/home/{user}/.config/autostart/colab.desktop", "w") as f:
+                f.write(colab_autostart)
+            os.system(f"chmod +x /home/{user}/.config/autostart/colab.desktop")
+            os.system(f"chown {user}:{user} /home/{user}/.config")
+            
+        os.system(f"adduser {user} chrome-remote-desktop")
+        command = f"{CRD_SSH_Code} --pin={Pin}"
+        os.system(f"su - {user} -c '{command}'")
+        os.system("service chrome-remote-desktop start")
+        
+        print(" ..........................................................")
+        print(" .....Brought By The Disala................................")
+        print(" ..........................................................")
+        print(" ......#####...######...####....####...##.......####.......")
+        print(" ......##..##....##....##......##..##..##......##..##......")
+        print(" ......##..##....##.....####...######..##......######......")
+        print(" ......##..##....##........##..##..##..##......##..##......")
+        print(" ......#####...######...####...##..##..######..##..##......")
+        print(" ..........................................................")
+        print(" ......... Telegram Channel - https://t.me/TheDisala4U ....")
+        print(" ..........................................................")
+        print(" ..Youtube Channel - https://www.youtube.com/@The_Disala ..")
+        print(" ..........................................................")
+        print("Log in PIN : 123456") 
+        print("User Name : user") 
+        print("User Pass : root") 
+        while True:
+            pass
 
-def crot(all):
-   options = webdriver.ChromeOptions()
-   options.add_argument("--headless")
-   options.add_argument( "--remote-debugging-pipe" )
-   options.add_experimental_option("excludeSwitches", ["enable-automation"])
-   options.add_experimental_option('useAutomationExtension', False)
-   options.add_argument("--disable-dev-shm-usage")
-   driver = webdriver.Chrome(options=options)
-
-
-   stealth(driver,
-           languages=["en-US", "en"],
-           vendor="Google Inc.",
-           platform="Win32",
-           webgl_vendor="Intel Inc.",
-           renderer="Intel Iris OpenGL Engine",
-           fix_hairline=True,
-           )
-
-
-
-   driver.get(all)
-   
-   wait = WebDriverWait(driver, 100)
-
-# Wait for the document to be fully loaded
-   wait.until(lambda driver: driver.execute_script("return document.readyState;") == "complete")
-
-#print(driver.page_source)
-   soup = BeautifulSoup(driver.page_source, "html.parser")
-
-   for dd in soup.select("script:-soup-contains('mydomain')"):
-     dd.decompose()
-
-   div = soup
-   """
-   if soup.find_all("div", {"id": "anu"}):
-     div = soup.find_all("div", {"id": "anu"})
-   else:
-     div = "hmm"
-   """
-   driver.close()
-   return str( div )
-@app.route("/")
-def ok():
-   
-   return crot(target)
-
-@app.route("/<path:all>" , strict_slashes=False, methods=['POST', 'GET'])
-def ull(all):
-   url = request.full_path
-   return crot(target+url)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=ports, debug=True)
+try:
+    if CRD_SSH_Code == "":
+        print("Please enter authcode from the given link")
+    elif len(str(Pin)) < 6:
+        print("Enter a pin more or equal to 6 digits")
+    else:
+        CRDSetup(username)
+except NameError as e:
+    print("'username' variable not found, Create a user first")
